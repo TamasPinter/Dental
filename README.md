90e95e756ad7331e3174d460393bba15469efee8
Tomas P <brolli_673@hotmail.com>
2023-11-05 12:12:23 -0500

    finished all content and style, ready to configure live launch

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ insta:
 https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png
 twitter:
 https://www.iconpacks.net/free-icon/twitter-logo-2429.html
+debridement:
+https://lowcountryfamilydentistry.com/wp-content/uploads/2021/03/Full-Mouth-Debridement.jpg
+polish:
+https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGpSfgDrXUTEk0WL5Cut5V7Br1XIRtb_U5Kw&usqp=CAU
+whitening:
+https://www.totaldentalcaremd.com/wp-content/uploads/man-smiling-teeth-whitening.jpg
+guard:
+https://www.memorialdental.ca/wp-content/uploads/2019/11/mouthguards-memorial-square-dental-ne-calgary-dentist.jpg
--- client/src/components/procedures.js
+++ client/src/components/procedures.js
@@ -1,5 +1,10 @@
 import React from "react";
 import procOne from "../assets/mouth.jpg";
+import procTwo from "../assets/debridement.jpg";
+import procThree from "../assets/polish.jpg";
+import procFour from "../assets/flouride.png";
+import procFive from "../assets/whitening.jpg";
+import procSix from "../assets/guard.jpg";
 
 function Procedures() {
   return (
@@ -7,36 +12,77 @@ function Procedures() {
       <h1>Here is a list of our procedures at a glance</h1>
       <div class="proceduresListTwo">
         <div class="procs">
+          <h3>Oral Hygenie Assessment</h3>
+
+          <div class="procDesc">
+            <img src={procOne} alt="oral hygenie assessment" />
+            <p>
+              An oral health assessment, oral cancer exam, cavity risk
+              assessment, dietary assessment.
+            </p>
+          </div>
+          <div class="cost">
+            <p> Estimated Cost: $54-$110</p>
+          </div>
+        </div>
+        <div class="procs">
+          <h3>Debridement</h3>
+          <div class="procDesc">
+            <img src={procTwo} alt="teeth debridement" />
+            <p>Removal of deposits using hand and/or ultrasonic instruments.</p>
+          </div>
+          <div class="cost">
+            <p>Estimated Cost: $63/per unit</p>
+          </div>
+        </div>
+        <div class="procs">
+          <h3>Selective Polish</h3>
+          <div class="procDesc">
+            <img src={procThree} alt="selective teeth polish" />
+            <p>Removal of remaining stain from affected teeth.</p>
+          </div>
+          <div class="cost">
+            <p> Estimated Cost: $34/per unit</p>
+          </div>
+        </div>
+        <div class="procs">
+          <h3>Flouride</h3>
+          <div class="procDesc">
+            <img src={procFour} alt="flouride treatement" />
+            <p>Topical application to strenghten and lower teeth decay.</p>
+          </div>
+          <div class="cost">
+            <p> Estimated Cost: $29</p>
+          </div>
+        </div>
+        <div class="procs">
           <h3>Teeth Whitening</h3>
-          <img src={procOne} alt="teeth whitening" />
-          <p>a little about the procedure</p>
+          <div class="procDesc">
+            <img src={procFive} alt="teeth whitening" />
+            <p>
+              Lighten and brighten appearance of smile, in office or using
+              custom trays for home use.
+            </p>
+          </div>
+          <div class="cost">
+            <p>Estimated Cost: In-office $54/unit or $162 for home kit.</p>
+          </div>
         </div>
         <div class="procs">
-          <h3>Teeth Cleaning</h3>
-          <img src={procOne} alt="teeth cleaning" />
-          <p>a little about the procedure</p>
-        </div>
-        <div class="procs">
-          <h3>Teeth Extraction</h3>
-          <img src={procOne} alt="teeth extraction" />
-          <p>a little about the procedure</p>
-        </div>
-        <div class="procs">
-          <h3>Teeth Filling</h3>
-          <img src={procOne} alt="teeth filling" />
-          <p>a little about the procedure</p>
-        </div>
-        <div class="procs">
-          <h3>Teeth X-Ray</h3>
-          <img src={procOne} alt="teeth x-ray" />
-          <p>a little about the procedure</p>
-        </div>
-        <div class="procs">
-          <h3>Teeth Stuff</h3>
-          <img src={procOne} alt="teeth stuff" />
-          <p>a little about the procedure</p>
+          <h3>Custom Sports Guard</h3>
+          <div class="procDesc">
+            <img src={procSix} alt="custom mouth guard" />
+            <p>Protects sports players from injury to oral structures.</p>
+          </div>
+          <div class="cost">
+            <p>Estimated Cost: $100</p>
+          </div>
         </div>
       </div>
+      <p>
+        Call us to book an appointment today or if you have any questions about
+        these services or others we may offer!
+      </p>
     </div>
   );
 }
